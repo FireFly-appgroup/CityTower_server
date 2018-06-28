ad689f23e4810ff4f873c913e645ec18736fabf3
Petr Lozhchenko <peetlucifer@gmail.com>
2018-06-28 18:18:22 +0300

    Adding start page and filters

--- CityTowerServer/CityTowerServer_API/Global.asax.cs
+++ CityTowerServer/CityTowerServer_API/Global.asax.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
+using CityTowerServer_API.App_Start;
 
 namespace CityTowerServer_API
 {
@@ -11,7 +9,10 @@ namespace CityTowerServer_API
     {
         protected void Application_Start()
         {
+            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
